48dd8521a80aece6888f09bb224abaf4b68568b6
Rajat <rjtthwl@gmail.com>
2023-01-10 13:34:23 +0530

    material added, header added

--- RT1/src/app/app-routing.module.ts
+++ RT1/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    component: HeaderComponent,
+    path: 'header'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
