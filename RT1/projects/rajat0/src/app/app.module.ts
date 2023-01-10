3a7a10979f978c4b3acbfbd2d1eb83b7fbc137c7
Rajat <rjtthwl@gmail.com>
2023-01-10 16:41:38 +0530

    notes list component created

--- RT1/projects/rajat0/src/app/app.module.ts
+++ RT1/projects/rajat0/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NotesListComponent } from './pages/notes-list/notes-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotesListComponent
   ],
   imports: [
     BrowserModule,
