8ee0a2fce7c20258deba4c5dcd215b72281e26e6
Rajat <rjtthwl@gmail.com>
2023-01-10 13:54:00 +0530

    abc

--- RT1/src/app/app-routing.module.ts
+++ RT1/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 
 const routes: Routes = [
+  // {
+  //   component: AppComponent,
+  //   path: ''
+  // },
   {
     component: HeaderComponent,
     path: 'header'
