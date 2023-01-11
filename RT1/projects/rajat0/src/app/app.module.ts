fecd39b201f274cb6c3d603997a3eac4216b16bd
Rajat <rjtthwl@gmail.com>
2023-01-11 10:55:56 +0530

    search, container, content, title, body

--- RT1/projects/rajat0/src/app/app.module.ts
+++ RT1/projects/rajat0/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NoteCardComponent } from './note-card/note-card.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainLayoutComponent } from './pages/main-layout/main-layout.component';
@@ -10,7 +10,8 @@ import { NotesListComponent } from './pages/notes-list/notes-list.component';
   declarations: [
     AppComponent,
     NotesListComponent,
-    MainLayoutComponent
+    MainLayoutComponent,
+    NoteCardComponent
   ],
   imports: [
     BrowserModule,
--- RT1/src/app/app.module.ts
+++ RT1/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { ContentComponent } from './content/content.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MainLayoutComponent } from '../../projects/rajat0/src/app/pages/main-layout/main-layout.component';
+import { NoteCardComponent } from './note-card/note-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ContentComponent,
-    MainLayoutComponent
+    MainLayoutComponent,
+    NoteCardComponent
   ],
   imports: [
     BrowserModule,
